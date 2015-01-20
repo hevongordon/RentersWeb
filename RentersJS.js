2647ebf4adf5b4c753080622bf07f602559b5f3b
hevon gordon <hevongordon@gmail.com>
2015-01-20 10:09:21 -0500

    added renters files to github

---
+++ RentersJS.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function load()
+{
+    $("#Slider").load("demos-no-jquery/image-gallery-with-vertical-thumbnail.source.html");
+}
+
+
